604ba3db4226e6244d77bea5dd51ce0780d7b7f6
m-somerville33 <masonsomerville33@gmail.con>
2026-03-05 10:32:45 -0700

    updated inventory so it should work

--- inventory.py
+++ inventory.py
@@ -1,21 +1,37 @@
+import db
+from models import Product, Electronics, Perishable
+
 class Inventory:
-    def __init__(self):
-        # Stores products using product_id as key
-        self.products = {}
-
-    # Add a product to inventory
+    # Add a product to the inventory
     def add_product(self, product):
-        self.products[product.product_id] = product
+        db.add_product(
+            product.product_id, 
+            product.name, 
+            product.price, 
+            product.stock_quantity
+        )
+        if isinstance(product, Electronics):
+            db.add_electronic(product.product_id, product.warranty_period)
+        elif isinstance(product, Perishable):
+            db.add_perishable(product.product_id, product.expiration_date)
 
-    # Remove a product from inventory
+
+    # Remove a product from the inventory
     def remove_product(self, product_id):
-        if product_id in self.products:
-            del self.products[product_id]
+        db.delete_product(product_id)
 
     # Get a product by its ID
-    def get_product(self, product_id):
-        return self.products.get(product_id)
+    def get_product_by_id(self, product_id):
+        row = db.get_product_by_id(product_id)
+        if row:
+            return Product(row['product_id'], row['name'], row['price'], row['stock_quantity'])
+        else:
+            return None
 
     # List all products in inventory
     def list_products(self):
-        return list(self.products.values())
+        rows = db.get_all_products()
+        products = []
+        for row in rows:
+            products.append(Product(row['product_id'], row['name'], row['price'], row['stock_quantity']))
+        return products
